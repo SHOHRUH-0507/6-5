83e2a9243500fd9483c239639c395399a083ab5a
shoxrux <shoxrux>
2025-09-07 21:10:08 +0500

    initialcommit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+    <h1 className="uppercase text-2xl text-title tracking-[1rem] leading-[1.5] text-center mb-10 lg:mb-22">
+      Spli
+      <br />
+      tter
+    </h1>
+
+    <div className="w-full max-w-[920px] rounded-t-3x1 p-8 bg-white "></div>
+     </div>
+  );
+}
+
+
+export default App
